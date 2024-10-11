10d19d7cf0b693aa6828e488ef909ff9f2ad55bb
Andrew Gaskell <gskll@protonmail.com>
2024-10-11 16:13:51 +0200

    separate user handlers to file

---
+++ internal/handlers/user.go
@@ -0,0 +1,140 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gskll/chirpy2/internal/auth"
+	"github.com/gskll/chirpy2/internal/database"
+	"github.com/gskll/chirpy2/internal/user"
+)
+
+func (router *APIRouter) RefreshToken(w http.ResponseWriter, r *http.Request) {
+	rToken, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+	dbToken, err := router.cfg.Db.GetRefreshToken(r.Context(), rToken)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusUnauthorized, err.Error())
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	if dbToken.ExpiresAt.Before(time.Now()) || dbToken.RevokedAt.Valid {
+		respondWithError(w, http.StatusUnauthorized, "token expired")
+		return
+	}
+
+	token, err := auth.MakeJWT(dbToken.UserID, router.cfg.JWTSecret, time.Hour)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"token": token})
+}
+
+func (router *APIRouter) RevokeRefreshToken(w http.ResponseWriter, r *http.Request) {
+	rToken, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+	err = router.cfg.Db.RevokeRefreshToken(r.Context(), rToken)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (router *APIRouter) CreateUser(w http.ResponseWriter, r *http.Request) {
+	type reqParams struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	params := reqParams{}
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	dbUser, err := router.cfg.Db.CreateUser(
+		r.Context(),
+		database.CreateUserParams{HashedPassword: hashedPassword, Email: params.Email},
+	)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	user := user.NewUser(dbUser)
+	respondWithJSON(w, http.StatusCreated, user)
+}
+
+func (router *APIRouter) LoginUser(w http.ResponseWriter, r *http.Request) {
+	type reqParams struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	params := reqParams{}
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	dbUser, err := router.cfg.Db.GetUserByEmail(r.Context(), params.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			respondWithError(w, http.StatusUnauthorized, "incorrect email or password")
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	err = auth.CheckPasswordHash(params.Password, dbUser.HashedPassword)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "incorrect email or password")
+		return
+	}
+
+	token, err := auth.MakeJWT(dbUser.ID, router.cfg.JWTSecret, time.Hour)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	refreshToken, err := auth.MakeRefreshToken()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	err = router.cfg.Db.CreateRefreshToken(
+		r.Context(),
+		database.CreateRefreshTokenParams{Token: refreshToken, UserID: dbUser.ID},
+	)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	user := user.NewUserWithTokens(dbUser, token, refreshToken)
+
+	respondWithJSON(w, http.StatusOK, user)
+}
