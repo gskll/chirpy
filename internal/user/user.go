6163ffb34fc117250247c7f3198a408342f6bfdd
Andrew Gaskell <gskll@protonmail.com>
2024-10-11 16:13:51 +0200

    add user type

---
+++ internal/user/user.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/gskll/chirpy2/internal/database"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func NewUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+}
